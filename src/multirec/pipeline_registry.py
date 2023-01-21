bfd2e44dd4a353c6a102266e6e1672690d47a25c
CapBlood <stalker.anonim@mail.ru>
2023-01-21 17:29:25 +0300

    feat (pipelines): добавлены пайплайны для добавления и обновления рекомендация в Mongo

--- src/multirec/pipeline_registry.py
+++ src/multirec/pipeline_registry.py
@@ -2,7 +2,10 @@
 from typing import Dict
 
 from kedro.framework.project import find_pipelines
+from kedro.pipeline.modular_pipeline import pipeline
 from kedro.pipeline import Pipeline
+
+from multirec.pipelines.build_recommendations_based_similar.pipeline import create_pipeline
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -13,4 +16,12 @@ def register_pipelines() -> Dict[str, Pipeline]:
     """
     pipelines = find_pipelines()
     pipelines["__default__"] = sum(pipelines.values())
+
+    build_rec_pipe = create_pipeline()
+    pipelines["add_recommendations_to_mongo"] = build_rec_pipe
+    pipelines["update_recommendations_to_mongo"] = pipeline(
+        pipe=build_rec_pipe,
+        inputs={"dataframe": "mongo_dataframe"}
+    )
+
     return pipelines
